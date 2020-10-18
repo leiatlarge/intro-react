283edbe60d8fe49500256157684056340a10afc6
Lei Shi <ls776@cornell.edu>
2020-10-18 14:03:32 -0700

    fix addStudent

--- src/App.jsx
+++ src/App.jsx
@@ -34,7 +34,7 @@ class App extends React.Component {
   }
 
   /*Write an addStudent method here*/
-  addStudent(c) {
+  addStudent(studentName) {
     this.setState({
       students: this.state.students.concat(studentName)
     });
